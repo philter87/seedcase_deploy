60c047901f62987a8e0a25a0f54367307ecd6d74
Philip Hjorth Christiansen <pch@mjolner.dk>
2023-12-12 10:40:24 +0100

    Remove vercel requirements and add migrate and test

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
+
 # Create your tests here.
+class HelloWorldTest(TestCase):
+    def testing_framework(self):
+        self.assertTrue(True, "Testing seems to work")
+
